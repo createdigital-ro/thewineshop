e4e79e49ee0c041a66a21cd5dc7b66d8d84428b8
shirinox <cosminsebastian2004@gmail.com>
2023-12-19 19:11:24 +0200

    fix: logo svg

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,5 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Navigation = () => {
-	return <div></div>;
+	return (
+		<div>
+			<Link href={'/'}>
+				<Image />
+			</Link>
+		</div>
+	);
 };
 
 export default Navigation;

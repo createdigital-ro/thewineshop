23dcbe62b11b7cead2bee556d3668e3a90cb98dc
shirinox <cosminsebastian2004@gmail.com>
2023-12-23 14:19:51 +0200

    feat: add page filtering

--- app/(public)/vinuri/page.tsx
+++ app/(public)/vinuri/page.tsx
@@ -41,7 +41,10 @@ const ShoppingPage = async ({ searchParams }: { searchParams: { id: number | und
 
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
 				{wines.map((wine) => {
-					return <ProductItem key={wine.id} wine={wine} />;
+					if (searchParams.id && wine.house.id == searchParams.id)
+						return <ProductItem key={wine.id} wine={wine} />;
+
+					if (!searchParams.id) return <ProductItem key={wine.id} wine={wine} />;
 				})}
 			</div>
 		</>

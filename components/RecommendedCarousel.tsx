cc627f9ef7403ccff42b2cf1c19c6cea8f20cd63
shirinox <cosminsebastian2004@gmail.com>
2023-12-23 21:45:44 +0200

    style: fixed recommended carousel viewport

--- components/RecommendedCarousel.tsx
+++ components/RecommendedCarousel.tsx
@@ -5,30 +5,28 @@ import ProductItem from './products';
 import { CompleteWine } from '@/prisma/zod';
 import Autoplay from 'embla-carousel-autoplay';
 
-const RecommendedCarousel = ({
-  recommendedWines,
-}: {
-  recommendedWines: CompleteWine[];
-}) => {
-  return (
-    <Carousel
-      plugins={[
-        Autoplay({
-          delay: 4000,
-        }),
-      ]}
-    >
-      <CarouselContent>
-        {recommendedWines.map((w, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
-            <ProductItem wine={w}></ProductItem>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  );
+const RecommendedCarousel = ({ recommendedWines }: { recommendedWines: CompleteWine[] }) => {
+	return (
+		<section className='max-w-[280px] mx-auto md:max-w-xl lg:max-w-4xl xl:max-w-7xl xl:mx-0'>
+			<Carousel
+				plugins={[
+					Autoplay({
+						delay: 4000,
+					}),
+				]}
+			>
+				<CarouselContent>
+					{recommendedWines.map((w, index) => (
+						<CarouselItem key={index} className='md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+							<ProductItem wine={w}></ProductItem>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</section>
+	);
 };
 
 export default RecommendedCarousel;

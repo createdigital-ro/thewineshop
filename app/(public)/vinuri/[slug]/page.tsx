27921ed75bd7a6f43be8ccc018901f82e36f0244
Alex-GHP <alex.morariu.dev@gmail.com>
2023-12-26 21:18:02 +0200

    add: single wine page

--- app/(public)/vinuri/[slug]/page.tsx
+++ app/(public)/vinuri/[slug]/page.tsx
@@ -1,5 +1,84 @@
-const WineProductPage = ({ params }: { params: { slug: string } }) => {
-	return <div>{params.slug}</div>;
+import { AddToCartButton } from "@/components/cart/ui";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Divider from "@/components/ui/Divider";
+
+const WineProductPage = async ({ params }: { params: { slug: string } }) => {
+  const currentYear = new Date().getFullYear();
+
+  const getML = (r: string) => {
+	let strWithDot = r.replace(",", ".")
+    return parseFloat(strWithDot) * 1000.0;
+  };
+
+  const wine = await prisma?.wine.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      house: true,
+      collection: true,
+    },
+  });
+
+  const caracteristics = [
+    {
+      title: "Vechime",
+      carac: currentYear - wine?.year + " ani",
+    },
+    {
+      title: "Casa",
+      carac: wine?.house.name,
+    },
+    {
+      title: "Colectie",
+      carac: wine?.collection.name,
+    },
+    {
+      title: "Cantitate",
+      carac:
+        parseFloat(wine?.litres) > 1
+          ? wine?.litres + " l"
+          : getML(wine?.litres) + " ml",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto mt-8">
+      <Image
+        src={wine?.image}
+        alt={`Photo of ${wine?.name}`}
+        width={700}
+        height={700}
+        className="mx-auto w-[700px] h-full object-cover"
+      />
+      <div className="md:pb-10">
+        <h2 className="font-bold text-center my-8 text-2xl sm:text-3xl md:text-4xl">
+          {wine?.name}
+        </h2>
+        <div className="grid grid-cols-2 my-8 gap-4">
+          {caracteristics.map((c, index) => (
+            <div key={index} className="leading-5 text-left mx-auto">
+              <p className="font-bold text-[1.25rem] sm:text-2xl">{c.title}</p>
+              <p className="text-zinc-600 font-semibold sm:text-[1.1rem]">
+                {c.carac}
+              </p>
+            </div>
+          ))}
+        </div>
+		<Divider />
+        <div className="px-10">
+          <p className="text-2xl font-semibold mb-1 sm:text-3xl">
+            {wine?.price} RON
+          </p>
+          <div className="flex justify-between items-center">
+            <Button className="font-bold text-[1.2rem]">Cumpara</Button>
+            <AddToCartButton wine={wine} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WineProductPage;
